ca6022a29d045e6f6a35cf90021b9e2fb1ba36be
Leila Inagas <leiladip33@gmail.com>
2024-10-18 13:29:31 -0300

    remove maintence validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
 import HomeLayout from "./ui/home/layout";
 import Home from "./ui/home/page";
-import Maintenance from "./ui/maintenance/maintenance";
 
 export default function HomeApp() {
-  const showMaintenance =
-    process.env.NODE_ENV !== "development" &&
-    process.env.MAINTENANCE_MODE === "true";
   return (
     <div>
-      {showMaintenance ? (
-        <Maintenance />
-      ) : (
-        <HomeLayout>
-          <Home />
-        </HomeLayout>
-      )}
+      <HomeLayout>
+        <Home />
+      </HomeLayout>
     </div>
   );
 }

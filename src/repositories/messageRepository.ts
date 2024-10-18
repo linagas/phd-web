5ab0e6986a27de50c8a6cad55439e503305db767
Leila Inagas <leiladip33@gmail.com>
2024-10-18 19:20:54 -0300

    API: fixes  y types

--- src/repositories/messageRepository.ts
+++ src/repositories/messageRepository.ts
@@ -1,30 +1,30 @@
-import { connectToDatabase } from "../utils/database";
+import { connectToDatabase } from "@/utils/database";
 import { Message } from "@/models/messageModel";
 
 export class MessageRepository {
-  private collectionName = "contact_messages";
+  private collectionName = "messages";
 
   async save(message: Message): Promise<void> {
-    const client = await connectToDatabase();
-    const db = client.db(process.env.MONGODB_DB);
-    const collection = db.collection(this.collectionName);
+    // Obtener cliente y base de datos a través de la conexión
+    const { db } = await connectToDatabase();
 
+    // Acceder a la colección y guardar el mensaje
+    const collection = db.collection(this.collectionName);
     await collection.insertOne(message);
   }
 
   async getAll(): Promise<Message[]> {
-    const client = await connectToDatabase();
-    const db = client.db(process.env.MONGODB_DB);
+    const { db } = await connectToDatabase();
     const collection = db.collection(this.collectionName);
 
-    const documents = await collection.find({}).toArray();
-    return documents.map(
-      (doc) =>
+    const results = await collection.find({}).toArray();
+    return results.map(
+      (result) =>
         ({
-          name: doc.name,
-          email: doc.email,
-          message: doc.message,
-          createdAt: doc.createdAt,
+          name: result.name,
+          email: result.email,
+          message: result.message,
+          createdAt: result.createdAt,
         } as Message)
     );
   }
